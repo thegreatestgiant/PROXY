59cd5ce9a69ebd468f5923e6cb13020180505b60
The Greatest Giant <96693544+thegreatestgiant@users.noreply.github.com>
2022-08-31 15:02:31 -0400

    Create wa.js

---
+++ wa.js
@@ -0,0 +1,5 @@
+document.addEventListener("keyup",function (e){var key=e.key+e.location;if(key=="Shift2")if(main.style.visibility=="hidden"){main.style.visibility="visible";console.style.visibility="visible"}else{main.style.visibility="hidden";console.style.visibility="hidden"}})
+var i=document.createElement('iframe');
+i.setAttribute('src',"https://alloyproxification.herokuapp.com/web/_aHR0cHM6Ly93ZWIud2hhdHNhcHAuY29t_/")
+i.setAttribute('style','position:relative;width:100%;height:100%;top:0px;right:0px;border:none;overflow:hidden');
+appid.appendChild(i)
