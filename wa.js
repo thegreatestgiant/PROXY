7e461fb3fb205562b21f8386f556f7ef1746851e
The Greatest Giant <96693544+thegreatestgiant@users.noreply.github.com>
2022-09-01 15:14:11 +0200

    Update and rename wap.js to wa.js

--- wa.js
+++ wa.js
@@ -1,5 +1,5 @@
 document.addEventListener("keyup",function (e){var key=e.key+e.location;if(key=="Shift2")if(main.style.visibility=="hidden"){main.style.visibility="visible";console.style.visibility="visible"}else{main.style.visibility="hidden";console.style.visibility="hidden"}})
 var i=document.createElement('iframe');
-i.setAttribute('src',"https://freecalculator2.herokuapp.com/main/https://web.whatsapp.com")
+i.setAttribute('src',"https://izhdh.sse.codesandbox.io/prefix/aHR0cHM6Ly93ZWIud2hhdHNhcHAuY29t/")
 i.setAttribute('style','position:relative;width:100%;height:100%;top:0px;right:0px;border:none;overflow:hidden');
 appid.appendChild(i)
