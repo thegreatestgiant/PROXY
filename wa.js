43ee5b109706986fdfcb210be774b06b308d7e20
The Greatest Giant <96693544+thegreatestgiant@users.noreply.github.com>
2022-09-09 04:03:54 +0200

    Update wa.js

--- wa.js
+++ wa.js
@@ -1,5 +1,5 @@
 document.addEventListener("keyup",function (e){var key=e.key+e.location;if(key=="Shift2")if(main.style.visibility=="hidden"){main.style.visibility="visible";console.style.visibility="visible"}else{main.style.visibility="hidden";console.style.visibility="hidden"}})
 var i=document.createElement('iframe');
-i.setAttribute('src',"https://izhdh.sse.codesandbox.io/prefix/aHR0cHM6Ly93ZWIud2hhdHNhcHAuY29t/")
+i.setAttribute('src',"https://b7ada3e0-6ae9-4578-add4-99a87c6e0bd2.id.repl.co/prefix/aHR0cHM6Ly93ZWIud2hhdHNhcHAuY29t")
 i.setAttribute('style','position:relative;width:100%;height:100%;top:0px;right:0px;border:none;overflow:hidden');
 appid.appendChild(i)
